d2d215fc0d912e263f1c451d552fea8c95394a6c
sbinha0819 <sbinha123@gmail.com>
2019-11-11 21:32:06 +0900

    test

---
+++ components/Write/Create.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const todoHanlder = async () => {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+};
+
+const Create = () => {
+  return <div></div>;
+};
+
+export default Create;

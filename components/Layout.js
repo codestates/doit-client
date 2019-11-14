06100ee8dc31dc140e5b104b1541ab6ab9ae9c01
sbinha0819 <sbinha123@gmail.com>
2019-11-14 22:52:16 +0900

    modified header

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Layout, Menu, Icon, Row, Col,
-} from 'antd';
+import { Layout, Menu, Icon, Row, Col } from 'antd';
 
 const { Header, Content } = Layout;
 
@@ -14,22 +12,22 @@ const DefaultLayout = ({ ...props }) => {
         <div className="logo" />
         <Row>
           <Col span={10}>
-            <Menu
-              theme="dark"
-              mode="horizontal"
-              style={{ lineHeight: '64px' }}
-            >
+            <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
               <Menu.Item key="1">
                 <Icon type="clock-circle" />
-                <a href="/timer" style={{ display: 'inline' }}>Timer</a>
+                <a href="/timer" style={{ display: 'inline' }}>
+                  Timer
+                </a>
               </Menu.Item>
               <Menu.Item key="2">
                 <Icon type="history" />
-                <a href="/todohistory" style={{ display: 'inline' }}>History</a>
+                <a href="/todohistory" style={{ display: 'inline' }}>
+                  History
+                </a>
               </Menu.Item>
             </Menu>
           </Col>
-          <Col push={10} span={4}>
+          <Col push={8} span={8}>
             {isLogin ? (
               <Menu
                 theme="dark"
@@ -53,11 +51,15 @@ const DefaultLayout = ({ ...props }) => {
               >
                 <Menu.Item key="3" onClick="">
                   <Icon type="login" />
-                  <a href="/login" style={{ display: 'inline' }}>Login</a>
+                  <a href="/login" style={{ display: 'inline' }}>
+                    Login
+                  </a>
                 </Menu.Item>
                 <Menu.Item key="4">
                   <Icon type="user-add" />
-                  <a href="/sign" style={{ display: 'inline' }}>Signup</a>
+                  <a href="/sign" style={{ display: 'inline' }}>
+                    Signup
+                  </a>
                 </Menu.Item>
               </Menu>
             )}

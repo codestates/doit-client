77142abe142e6f31096d22582566700e5606d344
양원석 <35683464+wonseokyang@users.noreply.github.com>
2019-11-14 22:34:24 +0900

    기능: 헤더 및 로그아웃 UI 제작
    로그아웃 UI 제작하면서 헤더도 추가하였음

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import { Layout } from 'antd';
+import React, { useState } from 'react';
+import {
+  Layout, Menu, Icon, Row, Col,
+} from 'antd';
 
-const DefaultLayout = ({ ...props }) => (
-  <Layout style={{ height: '100vh' }}>
-    <Layout.Header>​</Layout.Header>
-    <Layout.Content {...props} />
-    <Layout.Footer>​</Layout.Footer>
-  </Layout>
-);
+const { Header, Content } = Layout;
+
+const DefaultLayout = ({ ...props }) => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Header>
+        <div className="logo" />
+        <Row>
+          <Col span={10}>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">
+                <Icon type="clock-circle" />
+                <a href="/timer" style={{ display: 'inline' }}>Timer</a>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="history" />
+                <a href="/todohistory" style={{ display: 'inline' }}>History</a>
+              </Menu.Item>
+            </Menu>
+          </Col>
+          <Col push={10} span={4}>
+            {isLogin ? (
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                style={{ lineHeight: '64px' }}
+              >
+                <Menu.Item key="3" onClick="">
+                  <Icon type="logout" />
+                  Logout
+                </Menu.Item>
+                <Menu.Item key="4">
+                  <Icon type="user" />
+                  Subin Ha
+                </Menu.Item>
+              </Menu>
+            ) : (
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                style={{ lineHeight: '64px' }}
+              >
+                <Menu.Item key="3" onClick="">
+                  <Icon type="login" />
+                  <a href="/login" style={{ display: 'inline' }}>Login</a>
+                </Menu.Item>
+                <Menu.Item key="4">
+                  <Icon type="user-add" />
+                  <a href="/sign" style={{ display: 'inline' }}>Signup</a>
+                </Menu.Item>
+              </Menu>
+            )}
+          </Col>
+        </Row>
+      </Header>
+
+      <Content {...props} />
+    </Layout>
+  );
+};
 
 const FullLayout = ({ ...props }) => (
   <Layout>
-    <Layout.Content {...props} />
+    <Content {...props} />
   </Layout>
 );
 

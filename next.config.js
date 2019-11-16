3242f92073b500032b4a9818dfc8832c7e35c61a
LeeSuHo <angelxtry@gmail.com>
2019-11-16 15:31:37 +0900

    Delete all code

--- next.config.js
+++
@@ -1,1 +0,0 @@
-exports.default = { env: { GOOGLE_LOGIN_API: process.env.GOOGLE_LOGIN_API } };
--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import todoHistory from './todoHistory';
-
-const rootReducer = combineReducers({
-  todoHistory,
-});
-
-export default rootReducer;

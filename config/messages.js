732e977b87f546b161829518df84b37153721982
garahad <bfsudong@gmail.com>
2019-11-29 22:38:43 +0900

    conflic solve and message change

--- config/messages.js
+++ config/messages.js
@@ -2,13 +2,16 @@ const messages = {
   todoContentEmpty: '할일을 적어주세용',
   doneContentEmpty: '할일을 다 하셨군요! 결국 무슨 일을 했는지 입력해주세용',
   reset: '새로운 마음으로 다시 시작하시겠습니까?',
-  timerEnd: '시간이 종료되었습니다! 결국에 한 일, 다 못한 일, 간단한 반성 등을 적어주세용',
+  timerEnd:
+    '시간이 종료되었습니다! 결국에 한 일, 다 못한 일, 간단한 반성 등을 적어주세용',
   complete: '수고하셨네요! 진짜 조금 쉬시고 다시 스타트하세영',
-  writeTodo: '예시)\nReact 강의 한편 보기',
-  writeDone: '예시)\nReact 강의 한편 보기\n객체지향 디자인 패턴 공부하기\nHooks Tutorial 해보기\n\n오 많은 일을 했군 ^0^',
-  metaContent: '집중해서 쓴 시간과 그렇지 않은 시간의 차이는 큽니다. 두잇은 집중력과 시간관리를 위한 작지만 강력한 툴입니다.',
+  writeTodo: '예시)\nReact 강의 한편 보기\n런코 리액트 내용 보기',
+  writeDone:
+    '예시)\nReact 강의 한편 보기\n객체지향 디자인 패턴 공부하기\nHooks Tutorial 해보기\n\n오 많은 일을 했군 ^0^  다음엔 redux도 공부해야겠다. 폰을 끄니 집중이 잘된다.',
+  metaContent:
+    '집중해서 쓴 시간과 그렇지 않은 시간의 차이는 큽니다. 두잇은 집중력과 시간관리를 위한 작지만 강력한 툴입니다.',
   notLogout: '그래요. 더 열심히 하셔야죠!',
-  feedback: '자비 없는 피드백 기다립니다!'
+  feedback: '자비 없는 피드백 기다립니다!',
 };
 
 export default messages;
